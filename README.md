3f94530911c4d9d94498acd1a2930f214a22268c
ruijadom <ruijadomingues@gmail.com>
2020-11-23 11:02:04 +0000

    initial structure

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-typescript-webpack
+React starter
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Styles from "./styles.scss";
+
+const Header: React.FC = () => {
+  return (
+    <div className={Styles.header}>
+      <h2>React Starter</h2>
+    </div>
+  );
+};
+
+export default Header;
